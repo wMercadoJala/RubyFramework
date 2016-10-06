f19e587698c83439fa1f7f4f92d8242b297e280b
Walter Mercado <Walter.Mercado@jalasoft.com>
2016-10-06 11:05:36 -0400

    Update Project

--- src/requests/MeDetails/me_details_get.rb
+++ src/requests/MeDetails/me_details_get.rb
@@ -2,8 +2,8 @@ class MeDetailsGet
   def self.call_and_return_response(client, method)
     end_point = '/me'
     http_request = client.get_request(method, end_point)
-    http_response = client.execute_request(client.get_connection, http_request)
-    http_response
+    p http_response = client.execute_request(client.get_connection, http_request)
+    http_response 
   end
 
 end
